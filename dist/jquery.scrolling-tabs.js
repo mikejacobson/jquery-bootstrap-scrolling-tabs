488fcb163c80827eeaf2d72ad3bf17c461df034f
Mike Jacobson <michaeljjacobson1@gmail.com>
2017-05-11 19:34:33 -0700

    Update version

--- dist/jquery.scrolling-tabs.js
+++ dist/jquery.scrolling-tabs.js
@@ -1,6 +1,6 @@
 /**
  * jquery-bootstrap-scrolling-tabs
- * @version v0.7.1
+ * @version v1.0.0
  * @link https://github.com/mikejacobson/jquery-bootstrap-scrolling-tabs
  * @author Mike Jacobson <michaeljjacobson1@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
